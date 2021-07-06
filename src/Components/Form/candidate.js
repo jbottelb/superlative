78c72a502579b15a36dfb1066467fde9654514d6
Josh Bottelberghe <62488350+jbottelb@users.noreply.github.com>
2021-07-06 00:36:43 -0500

    Deleted Templates

--- src/Components/Form/candidate.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// creates a selector for each candidate
-const Candidate = ({ name }) => {
-  return <option value={name}>{name}</option>;
-};
-
-export default Candidate;
