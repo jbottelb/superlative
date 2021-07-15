3b8b2766748c79d23ddd683cea594b6a4f0e7363
Josh Bottelberghe <62488350+jbottelb@users.noreply.github.com>
2021-07-15 18:53:44 -0500

    setting up auth

---
+++ src/Services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// usually these would not be pushed, but this assignment needs
+// to be graded
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyDVlvjtJyAaYwTqYpZ9x8W6th7wcsX7XvE",
+  authDomain: "superlative-9227b.firebaseapp.com",
+  projectId: "superlative-9227b",
+  storageBucket: "superlative-9227b.appspot.com",
+  messagingSenderId: "1096265904136",
+  appId: "1:1096265904136:web:88c6214436c4c2f5eba1bd",
+  measurementId: "G-3E9S0DY7Q7"
+});
