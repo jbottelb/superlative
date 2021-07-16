9ebc30b830568f7c22eda75e219aacf89f02decf
Josh Bottelberghe <62488350+jbottelb@users.noreply.github.com>
2021-07-15 19:40:28 -0500

    update status

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,22 +5,21 @@ const Home = () => {
   return (
     <div>
       <h1> Project Status </h1>
+      <p>Feature 5 is being reworked. Right now, users can sign up.</p>
       <p>
-        Project status: Right now, for feature 4, the database is hooked up and
-        the routing is configured. The create forms is functioning, needs
-        authentification to put it in its final form. But it does create new
-        forms in the DB correctly. So far, this is the only example of correct
-        parse methods impliments. Routing works, and the vote feature currently
-        can direct to the right form. There is a get call which calls the db to
-        displau a form based on an id. Voting does not work but the
-        requireemnests for feature 4 on this have been filled.
+        The goal is to get a basic sign up service going, and then to have users
+        in back4app to be regestered with emails and own a list of the forms
+        they have created. Then I can use the current logged in user deal to add
+        to thier list of forms they have filled out. Then on a view forms page,
+        they can select from the forms they have created to see who is winning
+        what and such.
       </p>
       <h2>TODO</h2>
       <ul>
         <li>Add voting functionality (Connect from to update)</li>
         <li>Let users view the forms they have created</li>
         <li>Add styling</li>
-        <li>Authentification for creating forms</li>
+        <li>Authentification for creating forms and viewing</li>
         <li>Make home page an acutal home page, not a todo</li>
         <li>Write an about page</li>
       </ul>
