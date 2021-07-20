8f9641d3e72782169511a96a6ca998fdbec8d053
Josh Bottelberghe <62488350+jbottelb@users.noreply.github.com>
2021-07-20 15:39:16 -0500

    css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
 
 // root of the application
 const rootElement = document.getElementById("root");
