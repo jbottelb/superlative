f9c84bc2ff42562d89c302422c0cdd4812fc44b9
MackF <78003109+MackF@users.noreply.github.com>
2021-07-22 08:27:23 -0600

    test

--- src/Components/Vote/voter.js
+++ src/Components/Vote/voter.js
@@ -9,7 +9,7 @@ const Voter = () => {
   const formId = params["id"];
 
   const [forms, setForms] = useState([]);
-  const [name, setName] = useState();
+  //const [name, setName] = useState();
 
   // this will hold all the current votes on a candidate
   const [options, setOptions] = useState([]);
